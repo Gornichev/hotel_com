368e55692a80446b5c57582870f03c3d7dd72414
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2021-11-10 16:20:58 +0200

    burger menu start

--- int/js/common.js
+++ int/js/common.js
@@ -48,3 +48,20 @@ const feed = new Swiper(".feedback-slider", {
 });
 
 // burgermenu
+
+const btnMenu = document.querySelector("#btn-show-menu");
+const hideMenu = document.querySelector("#btn-hide-menu");
+const body = document.querySelector("body");
+const menu = document.querySelector("#menu");
+
+btnMenu.onclick = function() {
+    btnMenu.classList.add("hide");
+    body.classList.add("overlay");
+    menu.classList.add("show");
+}
+
+hideMenu.onclick = function() {
+    btnMenu.classList.remove("hide");
+    body.classList.remove("overlay");
+    menu.classList.remove("show");
+}
