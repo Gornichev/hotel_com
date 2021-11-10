e3bb095189682bd4572ae6dcaa058154283e3640
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2021-11-10 14:20:50 +0200

    section-feedback slider + auto-play

--- int/js/common.js
+++ int/js/common.js
@@ -1,20 +1,31 @@
+// Swiper Intro start
 const swiper = new Swiper(".in-intro-slider", {
     spaceBetween: 24,
     slidesPerView: 4,
     freeMode: true,
     watchSlidesProgress: true,
     direction: "vertical",
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
 });
 const swiper2 = new Swiper(".intro-slider", {
     spaceBetween: 24,
     thumbs: {
         swiper: swiper,
+
     },
 });
 // Swiper feedback start
 const swiperfeedback = new Swiper(".in-feedback-slider", {
     spaceBetween: 24,
     freeMode: true,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
 });
 const swiperinfeedback = new Swiper(".feedback-slider", {
     spaceBetween: 24,
@@ -25,6 +36,10 @@ const swiperinfeedback = new Swiper(".feedback-slider", {
         el: ".swiper-pagination",
         clickable: true,
         freeMode: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
     },
 });
 
